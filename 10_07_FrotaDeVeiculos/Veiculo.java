40b790e787093050f7301e966f8b68692eafc498
João Vitor Gaioski <140276793+joaovitorgaioski@users.noreply.github.com>
2025-10-07 22:21:24 -0300

    Add files via upload

---
+++ 10_07_FrotaDeVeiculos/Veiculo.java
@@ -0,0 +1,102 @@
+
+public abstract class Veiculo {
+
+    // Atributos
+    private String marca, modelo, placa;
+    private int quilometragem;
+    protected double litrosConsumidos;
+    StringBuilder statusConcatenado = new StringBuilder();
+
+    // Construtores
+    public Veiculo() {
+    }
+
+    public Veiculo(String marca, String modelo, String placa, int quilometragem) {
+        this.marca = marca;
+        this.modelo = modelo;
+        this.placa = placa;
+        this.quilometragem = quilometragem;
+    }
+
+    // Getters
+    public String getMarca() {
+        return this.marca;
+    }
+
+    public String getModelo() {
+        return this.modelo;
+    }
+
+    public String getPlaca() {
+        return this.placa;
+    }
+
+    public int getQuilometragem() {
+        return this.quilometragem;
+    }
+
+    // Setters
+    public void setMarca(String marca) {
+        if (!marca.isEmpty() && marca != "" && marca != null) {
+            this.marca = marca;
+        } else {
+            System.out.println("Erro: nome inválido");
+        }
+    }
+
+    public void setModelo(String modelo) {
+        if (!modelo.isEmpty() && modelo != "" && modelo != null) {
+            this.modelo = modelo;
+        } else {
+            System.out.println("Erro: nome inválido");
+        }
+    }
+
+    public void setPlaca(String placa) {
+        if (!placa.isEmpty() && placa != "" && placa != null) {
+            this.placa = placa;
+        } else {
+            System.out.println("Erro: nome inválido");
+        }
+    }
+
+    public void setQuilometragem(int quilometragem) {
+        if (quilometragem >= 0) {
+            this.quilometragem = quilometragem;
+        } else {
+            System.out.println("Erro: quilometragem inválida");
+        }
+    }
+
+    // Métodos
+    public void exibirStatus() {
+        App.linhaHorizontal();
+        System.out.println("Marca: " + getMarca());
+        System.out.println("Modelo: " + getModelo());
+        System.out.println("Placa: " + getPlaca());
+        System.out.println("Quilometragem: " + getQuilometragem() + " km rodados");
+    }
+
+    public void exibirStatusConcatenado() {
+        App.linhaHorizontal();
+
+        statusConcatenado.append(getMarca()).append(" ").append(getModelo()).append(", ").append(getPlaca()).append(", ").append(getQuilometragem()).append(" km");
+        statusConcatenado.toString();
+
+        System.out.println(statusConcatenado);
+        App.linhaHorizontal();
+    }
+
+    public void incrementarQuilometragem(int kmRodados) {
+        if (kmRodados >= 0) {
+            this.quilometragem += kmRodados;
+        } else {
+            System.out.println("Erro: quilometragem inserida inválida");
+        }
+    }
+
+    // Usei um método abstrato para forçar o método percorrer() nas filhas. As
+    // filhas apenas precisam da quantidade de km rodados, o valor km/litro é
+    // especializado
+    public abstract void percorrer(int kmRodados);
+}
