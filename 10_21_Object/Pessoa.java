cbcb94aa796b0bfb8497ccf3c720716ab6e78607
João Vitor Gaioski <jvgaioskI@hotmail.com>
2025-11-01 14:16:12 -0300

    Ajeitando algumas coisas

--- 10_21_Object/Pessoa.java
+++ 10_21_Object/Pessoa.java
@@ -1,12 +1,39 @@
 public class Pessoa {
     private String nome, idade, cpf;
 
+    // Construtor
     public Pessoa(String nome, String idade, String cpf) {
         this.nome = nome;
         this.idade = idade;
         this.cpf = cpf;
     }
 
+    // Getters e Setters
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getIdade() {
+        return idade;
+    }
+
+    public void setIdade(String idade) {
+        this.idade = idade;
+    }
+
+    public String getCpf() {
+        return cpf;
+    }
+
+    public void setCpf(String cpf) {
+        this.cpf = cpf;
+    }
+
+    // Métodos
     @Override
     public String toString() {
         return "Nome: " + nome + ", Idade: " + idade + ", CPF: " + cpf;
