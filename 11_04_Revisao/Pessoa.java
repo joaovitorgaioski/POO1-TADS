a376a6beba3dacf9a5ce7bf89f454f22e1e8172f
João Vitor Gaioski <140276793+joaovitorgaioski@users.noreply.github.com>
2025-11-04 22:04:21 -0300

    Add files via upload

--- 11_04_Revisao/Pessoa.java
+++ 11_04_Revisao/Pessoa.java
@@ -1,13 +1,16 @@
 public class Pessoa {
-    public static void main (String[] args){
-        String nome;
-        String cpf;
-        String endereco;
-        String formacaoEducacional;
-        int idade;
+    String nome;
+    String cpf;
+    String endereco;
+    String formacaoEducacional;
+    int idade;
+    
+    public void andar(int metros) {
+        System.out.println("A pessoa " + this.nome + " andou " + metros + "m.");
+    }
 
-        public void andar(int metros){
-            System.out.println("A pessoa " + this.nome + " andou "+ metros + "m");
-        }
+    public void aniversario(){
+            this.idade++;
+            System.out.println("Parabéns, hoje você faz " + this.idade + " anos!");
     }
 }
